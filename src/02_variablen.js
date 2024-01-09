2ca111cb4095b4f44e9400788341eaf75b819af2
bacaiib <155629559+bacaiib@users.noreply.github.com>
2024-01-09 10:51:05 +0000

    02 / Deklaration + Wertzuweisung 1

---
+++ src/02_variablen.js
@@ -0,0 +1,21 @@
+
+// Zeilenkommentar
+
+/*
+     Block-Kommentar
+
+*/
+
+// /***** Variablen 01 *******/
+
+// console.log("Hi!"); // Ausgabe
+// console.log(Hi); // Ausgabe
+
+/* Deklaration + Werstzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max";  // Wertzuweisung | assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName + "!");
