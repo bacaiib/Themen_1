955afae7e4f0fe577a62f7576bce3eaac56c9f6c
bacaiib <155629559+bacaiib@users.noreply.github.com>
2024-01-09 11:25:38 +0000

    02 /  Wertzuweisung II + Datentypen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -13,9 +13,33 @@
 
 /* Deklaration + Werstzuweisung I */
 
-let firstName; // Deklaration (Definition)
+/*let firstName; // Deklaration (Definition)
 console.log(firstName); // Ausgabe
 firstName = "Max";  // Wertzuweisung | assignment
 console.log(firstName); // Ausgabe
 let familyName = "Mütze"; // Deklaration + Wertzuweisung
 console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+*/
+/*  
+
+    Wertzuweisung II + Datentypen
+
+    JS ist ein untypisierte Sprache!
+
+*/
+
+let test;  // Deklaration
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------------");
+test = "hello"; // string
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------------");
+test = 2 // number
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("-------------");
+test = true; // boolean
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
