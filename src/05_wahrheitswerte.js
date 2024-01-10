eeabadb015c2bb3ca620e1a023452bf1787094cf
bacaiib <155629559+bacaiib@users.noreply.github.com>
2024-01-10 11:33:42 +0000

    05 / Berechnungs Variante 1

---
+++ src/05_wahrheitswerte.js
@@ -0,0 +1,14 @@
+/* Berechnung Alter */
+
+// Deklaration
+let ageJohn, ageMark;
+const birthYearJohn = 2000;
+const birthYearMark = 1990;
+
+// Berechnung Alter
+ageJohn = 2024 - birthYearJohn;
+ageMark = 2024 - birthYearMark;
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
