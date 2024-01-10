2a791599bea9abdf82b142cec43a79fc982b3770
bacaiib <155629559+bacaiib@users.noreply.github.com>
2024-01-10 11:52:17 +0000

    05 / Wahrheitswerte Variante 2

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -6,9 +6,13 @@ const birthYearJohn = 2000;
 const birthYearMark = 1990;
 
 // Berechnung Alter
+let date = new Date(); // Systemfunktion Datum
+let year = date.getFullYear(); // Daten liefern lassen!!
 ageJohn = 2024 - birthYearJohn;
 ageMark = 2024 - birthYearMark;
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
+
+console.log("Datum: " + date);
