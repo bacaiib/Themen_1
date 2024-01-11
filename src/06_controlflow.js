237c8fd5ac0f75d915c0e6170e8d6110470fc060
bacaiib <155629559+bacaiib@users.noreply.github.com>
2024-01-11 09:48:42 +0000

    06 / IF_ELSE

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,7 +1,7 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 32;
+const ageJohn = 30;
 const ageMark = 30;
 
 // Deklaration
@@ -26,7 +26,20 @@ console.log("------------------");
 // if (true)
 // if (false)
 // if (isJohnOlder)
-if(ageJohn > ageMark)
+// if(ageJohn > ageMark)
+// {
+//     console.log("John ist älter!");
+// }
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+if (isJohnOlder) 
 {
     console.log("John ist älter!");
+} 
+else 
+{
+    console.log("John ist nicht älter!");
 }
