b3c17cb7fbdf441e4f9f54f43e7a97462404cc00
bacaiib <155629559+bacaiib@users.noreply.github.com>
2024-01-12 09:14:33 +0000

    08 / 1. Kapselung von Codeblöcken

---
+++ src/08 _funktionen_I.js
@@ -0,0 +1,12 @@
+/***** Funktionen 01  *****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf | call
+test();
+
+// Fuktionsrumpf | callee
+// Funktionsdeklaration
+function test ()
+{
+    console.log("Hallo, Benjamin!");
+}
