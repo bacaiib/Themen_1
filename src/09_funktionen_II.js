24c18e2d825efd780aa6383074fc26746d8da68d
bacaiib <155629559+bacaiib@users.noreply.github.com>
2024-01-15 09:50:09 +0000

    09 / Divide () / var. 1

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -36,6 +36,20 @@ function mul(a,b) {
 	return a * b;
 }
 
+// module: divide a / b | Test
+
+output(div(2,2));
+output(div(2,2));
+output(div(2,0));
+
+function div(a,b) {
+
+	if (b !== 0) {  // b ungleich 0
+		return a / b;
+	} else {
+		return "Division durch nUll geht nicht yaaa"
+	}
+}
 
 // module: output | test:
 // output("hello");
