be3eab3eee7d0417d8fc850efc59ba691d723d53
bacaiib <155629559+bacaiib@users.noreply.github.com>
2024-01-16 11:34:11 +0000

    11 / Arrays 1

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -6,7 +6,17 @@
  * 
  */
 
+/*** 02. Theorie: Array */
+// let arr;
+// arr = New Array(); // Konstruktor
+// arr = []; // Literal
+// arr = [2,11,7,8,10];
+// arr = [2,]
 
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Pos. im Array)
+// output(arr[arr.length-1]); // letzte Pos. / allgemein
 
 /**
  *   PRAXIS
@@ -18,19 +28,41 @@
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4) {
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 + 
                 PUNCT;
 
     return str;
 }
 
-
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+       const GAP = " ";
+       const PUNCT = ".";
+       let str =   arr[0] + GAP +
+                   arr[1] + GAP +
+                   arr[2] + GAP +
+                   arr[3] + GAP +
+                   arr[4] + GAP +
+                   arr[5] + 
+                   PUNCT;
+   
+   
+   
+       return str;
+   }
 
 
 // Modul: Ausgabe in Konsole : Test
