275bca6838c6ee91b8da8a24008bd285d20386a3
bacaiib <155629559+bacaiib@users.noreply.github.com>
2024-01-19 08:30:16 +0000

    14 / Objecte 1

--- src/14_Objects.js
+++ src/14_Objects.js
@@ -2,41 +2,63 @@
 
 
 
-let arr;
-arr = ["Ich","bin","Max"];
-output(arr);
-output(arr[0]);
-output("------------");
+// let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
 
 // nested arrays - Prinzip: [[[]]]
 // nested arrays  - hier: [[],[]]
-arr = [
-    ["Ich","bin","Max"],
-    ["Ich","bin","Moritz"]
-        ];
+// arr = [
+//     ["Ich","bin","Max"],
+//     ["Ich","bin","Moritz"]
+//         ];
 
 
 
-output(arr);
-output(arr[0]);
-output(arr[0][0]+ " " + arr[1][2]);
-output(arr[1][0]);
-output("------------");
+// output(arr);
+// output(arr[0]);
+// output(arr[0][0]+ " " + arr[1][2]);
+// output(arr[1][0]);
+// output("------------");
 
 // nested loop
 
-for (let i = 0; i < arr.length; i++) {
+// for (let i = 0; i < arr.length; i++) {
 
-    for (let j = 0; j < arr[i].length; j++) {
+//     for (let j = 0; j < arr[i].length; j++) {
 
-        output("Index i: " + i + " Index j: " + j);
+//         output("Index i: " + i + " Index j: " + j);
 
-        output(arr[i][j]);      
+//         output(arr[i][j]);      
 
-    }
+//     }
 
-}
+// }
 
+/***** Objekte 1 Daten/Funktionen *******/
+
+let person = {
+                firstName: "Bahrang",
+                familyName: "Valadkhani",
+                salary: [120000, 160000],
+                premission: true,
+                sayHello: function(){
+                    return "Hello";
+                }
+
+
+                };
+
+// output(person);
+output(person.familyName);
+output(person.salary[1]);
+output(person.sayHello());
+
+const txt = person.sayHello() + " Ich bin " + person.firstName + " " + person.familyName + " und verdiene " + person.salary[1] + " p.a."
+
+output(txt);
 function output(outputData) {
     console.log(outputData);
 }
